02ccc857cee2451799e5f7361add4070765b29ef
FawwazAfkar <fawwaz.muzakky@mhs.unsoed.ac.id>
2024-07-24 11:47:43 +0700

    add routes + mods layout app

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,14 @@
 <?php
 
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ChangePasswordController;
+use Illuminate\Support\Facades\Auth; 
+
 
 Route::get('/', function () {
-    return view('auth/login');
+    return view('welcome');
 });
 
 Auth::routes();
@@ -14,3 +18,7 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/daftar-user', [HomeController::class, 'daftarUser'])->name('daftar-user');
+Route::get('/daftar-arsip', [HomeController::class, 'daftarAV'])->name('daftar-arsip');
+Route::get('/daftar-instansi', [HomeController::class, 'daftarInstansi'])->name('daftar-instansi');
